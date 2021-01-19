d22cdb5aef7874ed705001f900cc09fda326ee1a
Khamis Hesham <khamis1997.kh@gmail.com>
2021-01-19 17:46:02 +0200

    Solve Exercise-3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 
-import One from "./components/Exercise-1";
-import Two from "./components/Exercise-2";
+// import One from "./components/Exercise-1";
+// import Two from "./components/Exercise-2";
+import Three from "./components/Exercise-3";
 
 function App() {
   return (
     <div className="App">
-      <One />
-      <Two />
+      {/* <One />
+      <Two /> */}
+      <Three />
     </div>
   );
 }
